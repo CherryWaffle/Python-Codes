c9d914c43c70919bb7dd3fc56651cfec8a8f2d26
ranjithh-github <72537594+CherryWaffle@users.noreply.github.com>
2022-01-26 23:02:12 +0530

    Works !

---
+++ Python-Codes/Temperature Converter/Converter.py
@@ -0,0 +1,8 @@
+# Celcius to Farenheit
+
+Cels = float(input("Enter the temperature in Celcius:"))
+
+print("Temperature in Celcius(°C) is:", Cels, "°C")
+Fahr = Cels*9/5+32
+
+print ("Temperature is Farenheit(°F) is:", Fahr, "°F")
