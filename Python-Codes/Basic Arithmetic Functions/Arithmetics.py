390ca21eb0690c573a73fd251bb56f2e38b6ecfb
ranjithh-github <72537594+CherryWaffle@users.noreply.github.com>
2022-01-27 15:11:13 +0530

    Works !

---
+++ Python-Codes/Basic Arithmetic Functions/Arithmetics.py
@@ -0,0 +1,15 @@
+x = int(input("Enter Value 1: "))
+y = int(input("Enter Value 2: "))
+
+summ = x + y
+diff = x - y
+mult = x * y
+div = x / y
+mod = x % y
+
+print("Numbers are :", x, "and", y)
+print("Sum = ",summ)
+print("Difference = ", diff)
+print("Product = ",mult)
+print("Division = ", div)
+print("Modulus =", mod)
