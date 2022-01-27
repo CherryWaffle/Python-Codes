db3185e1f49ed6adbebef8d5d755d7f909c6958f
ranjithh-github <72537594+CherryWaffle@users.noreply.github.com>
2022-01-27 15:25:03 +0530

    Works !

---
+++ Python-Codes/Avergae Finder/Averagecalc.py
@@ -0,0 +1,7 @@
+x = int(input("Enter Value 1: "))
+y = int(input("Enter Value 2: "))
+z = int(input("Enter Value 3: "))
+print("The Three NUmbers Are :", x, y, z)
+
+avg = (x + y + z)/3
+print("Average Of given Numbers Is", avg)
