bb1c23766ad25d6fd23e1d7c346cb3faf34f711b
ranjithh-github <72537594+CherryWaffle@users.noreply.github.com>
2022-02-01 22:50:33 +0530

    Works !

---
+++ Python-Codes/Random Number/generator.py
@@ -0,0 +1,12 @@
+import math
+import random
+import random
+import math
+
+fnum = random.random() * (95 - 45) + 45
+inum = math.ceil( fnum )
+
+print("Random numbers between 45 .. 95 :")
+print(fnum)
+
+print("Nearest Higher Integer :", inum)
