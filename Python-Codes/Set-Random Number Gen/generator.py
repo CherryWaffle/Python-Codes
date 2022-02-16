9dd4bce41dc055abb5d44143877caab4fda4f762
ranjithh-github <72537594+CherryWaffle@users.noreply.github.com>
2022-02-16 23:24:49 +0530

    Create generator.py

---
+++ Python-Codes/Set-Random Number Gen/generator.py
@@ -0,0 +1,11 @@
+import random as rand
+
+num1=rand.randrange(10, 70, 13)
+num2=rand.randrange(10, 70 ,13)
+num3=rand.randrange(10, 70, 13)
+
+set1 = {num1, num2, num3}
+print("Random Integers in the range 10 .. 70,\
+    setp-value 13 :", num1, num2, num3)
+
+print("Set created :", set1)
