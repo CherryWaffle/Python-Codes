01e0dadd9900612a1c7e95d4a4aeed40b96d25df
ranjithh-github <72537594+CherryWaffle@users.noreply.github.com>
2022-02-16 23:25:49 +0530

    Works !

--- Python-Codes/Set-Random Number Gen/generator.py
+++ Python-Codes/Set-Random Number Gen/generator.py
@@ -5,7 +5,7 @@ num2=rand.randrange(10, 70 ,13)
 num3=rand.randrange(10, 70, 13)
 
 set1 = {num1, num2, num3}
-print("Random Integers in the range 10 .. 70,\
-    setp-value 13 :", num1, num2, num3)
+print("Random Integers in the range 10 .. 70, \
+    step-value 13 :", num1, num2, num3)
 
 print("Set created :", set1)
