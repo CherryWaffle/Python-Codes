5eea799cd5289b8759cf297d23e8b61b1d64672f
ranjithh-github <72537594+CherryWaffle@users.noreply.github.com>
2022-02-23 23:26:42 +0530

    Works !

--- Python-Codes/Divisibility Test/tester.py
+++ Python-Codes/Divisibility Test/tester.py
@@ -1,10 +1,10 @@
-number1 = int(("Enter first number : "))
-number2 = int(("Enter second number : "))
+number1 = int(input("Enter first number : "))
+number2 = int(input("Enter second number : "))
 
-remainder = number1 / number2
+remainder = number1 % number2
 
 if remainder == 0 :
     print(number1, "is divisible by", number2)
 
 else : 
-    print(number1, "is not divisible by", number2)
+    print(number1, "is not divisible by", number2, "and", remainder, "is the remainder")
