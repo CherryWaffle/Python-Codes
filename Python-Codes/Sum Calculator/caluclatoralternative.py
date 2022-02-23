c6057cdde8136b4aed3354e9f3258ad3441192ea
ranjithh-github <72537594+CherryWaffle@users.noreply.github.com>
2022-02-23 23:15:20 +0530

    Not Working !

---
+++ Python-Codes/Sum Calculator/caluclatoralternative.py
@@ -0,0 +1,26 @@
+sum1 = sum2 = 0
+
+num1 = int(input("Enter number one : "))
+num2 = int(input("Enter number two : "))
+num3 = int(input("Enter number three : "))
+
+sum1 = num1 + num2 + num3
+
+if num1 == num2 :
+    if num3 != num1 :
+        sum2 += num3
+
+else :
+    if num1 == num3 :
+    sum2 += num2
+
+  else :
+      if num2 == num3 :
+          sum2 += num1
+
+    else :
+        sum2 += num1 + num2 + num3
+
+print("Numbers are", num1, num2, num3)
+print("Sum of the three given numbers is : ", sum1)
+print("Sum of the non-duplicate numbers is : ", sum2)
